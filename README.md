f7f86ced051f52bffa902df44401c5891376a202
Fajla Rabby <blfajlarabby414@gmail.com>
2024-10-17 20:58:26 +0600

    http-errors

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 
 1. Course plan
 2. Environment setup
-3. Create expreess server
+3. Create expreess server -> express
 4. HTTP request & response
-5. nodemon and morgan package
+5. nodemon and morgan package  -> nodemon, morgan
 6. API testing with Postman
 7. Middleware Types of Middleware
-8. Express Error Handling Middleware
-9. How to handle HTTP erros
+8. Express Error Handling Middleware -> body-parser
+9. How to handle HTTP erros ->  http-errors
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const createError = require('http-errors');
 
 const app = express();
 app.use(morgan('dev'));
@@ -35,17 +36,16 @@ app.get('/api/user', isLoggedIn, (req, res) => {
 
 // client error handling
 app.use((req, res, next) => {
-    res.status(404).send({
-        message: 'route not found'
-    })
-    next();
+    next(createError(404, 'route not found'));
 })
 
 
 // server error handling
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
+    return res.status(err.status || 500).json({
+        success: false,
+        message: err.message,
+    })
 })
 
 
